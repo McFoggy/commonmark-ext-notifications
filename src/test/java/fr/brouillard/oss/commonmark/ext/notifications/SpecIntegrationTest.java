aba416a4de78c7ed29e908e48fede4bd34dbc4be
Matthieu Brouillard <matthieu@brouillard.fr>
2020-06-08 18:44:38 +0200

    update commonmark-java dependency

--- src/test/java/fr/brouillard/oss/commonmark/ext/notifications/SpecIntegrationTest.java
+++ src/test/java/fr/brouillard/oss/commonmark/ext/notifications/SpecIntegrationTest.java
@@ -9,7 +9,7 @@ import org.commonmark.Extension;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.commonmark.testutil.SpecTestCase;
-import org.commonmark.testutil.spec.SpecExample;
+import org.commonmark.testutil.example.Example;
 import org.junit.Test;
 
 /**
@@ -26,7 +26,7 @@ public class SpecIntegrationTest extends SpecTestCase {
     private static final HtmlRenderer RENDERER = HtmlRenderer.builder().extensions(EXTENSIONS).percentEncodeUrls(true).build();
     private static final Map<String, String> OVERRIDDEN_EXAMPLES = getOverriddenExamples();
 
-    public SpecIntegrationTest(SpecExample example) {
+    public SpecIntegrationTest(Example example) {
         super(example);
     }
 
